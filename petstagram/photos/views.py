26afe9e4c81cb84462c16c7305f1a3415998beb2
GeorgiDN <go6odn28@gmail.com>
2024-11-20 22:02:07 +0200

    feat: create comment form

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 
+from petstagram.common.forms import CommentForm
 from petstagram.photos.forms import PhotoAddForm, PhotoEditForm
 from petstagram.photos.models import Photo
 
@@ -28,6 +29,7 @@ def photo_details_page(request, pk):
     photo = Photo.objects.get(pk=pk)
     likes = photo.photo_likes.all()
     comments = photo.photo_comments.all()
+    comment_form = CommentForm()
 
     context = {
         "photo": photo,
