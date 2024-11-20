db72569e14dd1b3d2b8543806b7316a9adb9e405
GeorgiDN <go6odn28@gmail.com>
2024-11-20 19:42:54 +0200

    feat: add photo-details-page

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 
+from petstagram.photos.models import Photo
+
 
 def photo_add_page(request):
     return render(request, "photos/photo-add-page.html")
 
 
 def photo_details_page(request, pk):
-    return render(request, "photos/photo-details-page.html")
+    photo = Photo.objects.get(pk=pk)
+    likes = photo.photo_likes.all()
+    comments = photo.photo_comments.all()
+
+    context = {
+        "photo": photo,
+        "likes": likes,
+        "comments": comments,
+    }
+
+    return render(request, "photos/photo-details-page.html", context)
 
 
 def photo_edit_page(request, pk):
