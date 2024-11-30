1cee703fc2eecfd81e8c9b4a0785750c8d93ceed
GeorgiDN <go6odn28@gmail.com>
2024-11-30 16:55:09 +0200

    feat: create CBV PhotoAddPage and PhotoEditPage

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
 
 from petstagram.common.forms import CommentForm
 from petstagram.photos.forms import PhotoAddForm, PhotoEditForm
 from petstagram.photos.models import Photo
 
 
-def photo_add_page(request):
-    form = PhotoAddForm(request.POST or None, request.FILES or None)
+class PhotoAddPage(CreateView):
+    model = Photo
+    form_class = PhotoAddForm
+    template_name = "photos/photo-add-page.html"
+    success_url = reverse_lazy("home")
 
-    if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            return redirect("home")
 
-    context = {
-        "form": form,
-    }
-
-    return render(request, "photos/photo-add-page.html", context)
+# def photo_add_page(request):
+#     form = PhotoAddForm(request.POST or None, request.FILES or None)
+#
+#     if request.method == "POST":
+#         if form.is_valid():
+#             form.save()
+#             return redirect("home")
+#
+#     context = {
+#         "form": form,
+#     }
+#
+#     return render(request, "photos/photo-add-page.html", context)
 
 
 def photo_delete(request, pk):
@@ -40,19 +49,28 @@ def photo_details_page(request, pk):
     return render(request, "photos/photo-details-page.html", context)
 
 
-def photo_edit_page(request, pk):
-    photo = Photo.objects.get(pk=pk)
-    # form = PhotoAddForm(request.POST or None, request.FILES or None, instance=photo)
-    form = PhotoEditForm(request.POST or None, instance=photo)
+class PhotoEditPage(UpdateView):
+    model = Photo
+    form_class = PhotoEditForm
+    template_name = "photos/photo-edit-page.html"
 
-    if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            return redirect("photo-details", pk)
+    def get_success_url(self):
+        return reverse_lazy("photo-details", kwargs={"pk": self.object.pk})
 
-    context = {
-        "form": form,
-        "photo": photo,
-    }
 
-    return render(request, "photos/photo-edit-page.html", context)
+# def photo_edit_page(request, pk):
+#     photo = Photo.objects.get(pk=pk)
+#     # form = PhotoAddForm(request.POST or None, request.FILES or None, instance=photo)
+#     form = PhotoEditForm(request.POST or None, instance=photo)
+#
+#     if request.method == "POST":
+#         if form.is_valid():
+#             form.save()
+#             return redirect("photo-details", pk)
+#
+#     context = {
+#         "form": form,
+#         "photo": photo,
+#     }
+#
+#     return render(request, "photos/photo-edit-page.html", context)
