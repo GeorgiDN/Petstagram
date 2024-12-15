5431be5457e2169b24af6e4a809cef7085470caf
GeorgiDN <go6odn28@gmail.com>
2024-12-15 11:54:32 +0200

    feat: register functionality

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,12 +1,32 @@
+from django.contrib.auth import get_user_model, login
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from petstagram.accounts.forms import AppUserCreationForm
+
+UserModel = get_user_model()
 
 
-def login(request):
-    return render(request, "accounts/login-page.html")
+# def login(request):
+#     return render(request, "accounts/login-page.html")
 
 
-def register(request):
-    return render(request, "accounts/register-page.html")
+class AppUserRegisterView(CreateView):
+    model = UserModel
+    form_class = AppUserCreationForm
+    template_name = "accounts/register-page.html"
+    success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        login(self.request, self.object)
+        return response
+
+
+# def register(request):
+#     return render(request, "accounts/register-page.html")
 
 
 def profile_delete(request, pk):
